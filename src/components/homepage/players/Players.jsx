649c8b4aee820917f43b0a0d243a9408a0f49cdf
Fahmida Akter Shimu <fahmidaaktershimu@gmail.com>
2026-03-28 17:38:42 +0600

    State Management & Toggle Functionality

--- src/components/homepage/players/Players.jsx
+++ src/components/homepage/players/Players.jsx
@@ -1,16 +1,33 @@
-import React, { use } from 'react';
+import React, { use, useState } from 'react';
 import AvailablePlayers from './availablePlayers/AvailablePlayers';
+import SelectedPlayers from './selectedPlayers/SelectedPlayers';
 
 const Players = ({ playersPromise }) => {
-    console.log(playersPromise)
 
     const players = use(playersPromise);
-    console.log(players);
+
+    const [active, setActive] = useState("available");
 
     return (
-        <div className='max-w-330 mx-auto'>
-            {/* players: {players.length} */}
-            <AvailablePlayers players={players}></AvailablePlayers>
+        <div className='max-w-330 mx-auto mt-20'>
+
+            <div className='flex justify-between items-center mb-8'>
+                {active === "available" ? <h2 className='text-3xl font-bold'>Available players</h2> : <h2 className='text-3xl font-bold'>Selected Player (4/6)</h2>}
+
+                <div>
+                    <button
+                        onClick={() => setActive("available")}
+                        className={`btn ${active === "available" ? "font-bold bg-[#E7FE29]" : "opacity-60 bg-white"} rounded-l-xl rounded-r-none`}>Available</button>
+
+                    <button
+                        onClick={() => setActive("selected")}
+                        className={`btn ${active === "selected" ? "font-bold bg-[#E7FE29]" : "opacity-60 bg-white"} rounded-r-xl rounded-l-none`}>Selected <span>(0)</span></button>
+                </div>
+            </div>
+
+            {active === "available"
+                ? <AvailablePlayers players={players}></AvailablePlayers>
+                : <SelectedPlayers></SelectedPlayers>}
         </div>
     );
 };
